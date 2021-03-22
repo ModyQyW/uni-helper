eac77384955d9f04a614540d4eea0b7fc6038844
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-03-22 23:26:38 +0800

    chore(vscode-uni-app-schemas): v0.3.1

--- extensions/vscode-uni-app-schemas/README.md
+++ extensions/vscode-uni-app-schemas/README.md
@@ -17,5 +17,5 @@
 - [前端学习路径](https://modyqyw.top/front-end/roadmap/)
 - [前端环境配置](https://modyqyw.top/front-end/environment/)
 - [前端杂项](https://modyqyw.top/front-end/misc/)
-- [fabric](https://github.com/modyqyw/fabric#readme) - 不同项目的共享配置
+- [fabric](https://github.com/modyqyw/fabric#readme) - 不同前端项目的可共享规范
 - [mp-scss](https://modyqyw.top/mp-scss/) - 一个基于 Flexbox 的小程序 SCSS 样式库，用于快速实现自定义设计
--- extensions/vscode-uni-app-schemas/generate.js
+++ extensions/vscode-uni-app-schemas/generate.js
@@ -1,31 +1,26 @@
 import fs from 'fs';
 
-let readmeData = '';
+const readme = `# vscode-uni-app-schemas
 
-// 添加标题
-readmeData += '# vscode-uni-app-schemas\n\n';
+## 插件特性
 
-// 添加插件特性
-readmeData += '## 插件特性\n\n';
-readmeData += '- 校验 uni-app `pages.json` 和 `manifest.json` 格式\n\n';
-readmeData +=
-  '本插件是 [uni-helper](https://marketplace.visualstudio.com/items?itemName=ModyQyW.vscode-uni-helper) 的一部分。本插件和文档的冲突之处，请以文档为准。\n\n';
-readmeData +=
-  '欢迎提交 [ISSUE](https://github.com/ModyQyW/uni-helper/issues/new) 和 [PR](https://github.com/ModyQyW/uni-helper/compare) 改进本插件。\n\n';
+- 校验 uni-app \`pages.json\` 和 \`manifest.json\` 格式
 
-// 添加使用
-readmeData += '## 使用\n\n';
-readmeData +=
-  '安装插件后重启 VSCode，打开对应的文件，如果有问题就会看到提示。\n\n';
+本插件是 [uni-helper](https://marketplace.visualstudio.com/items?itemName=ModyQyW.vscode-uni-helper) 的一部分。本插件和文档的冲突之处，请以文档为准。
 
-// 添加更多
-readmeData += '## 更多\n\n';
-readmeData += '- [前端学习路径](https://modyqyw.top/front-end/roadmap/)\n';
-readmeData += '- [前端环境配置](https://modyqyw.top/front-end/environment/)\n';
-readmeData += '- [前端杂项](https://modyqyw.top/front-end/misc/)\n';
-readmeData +=
-  '- [fabric](https://github.com/modyqyw/fabric#readme) - 不同项目的共享配置\n';
-readmeData +=
-  '- [mp-scss](https://modyqyw.top/mp-scss/) - 一个基于 Flexbox 的小程序 SCSS 样式库，用于快速实现自定义设计\n';
+欢迎提交 [ISSUE](https://github.com/ModyQyW/uni-helper/issues/new) 和 [PR](https://github.com/ModyQyW/uni-helper/compare) 改进本插件。
 
-fs.writeFileSync('README.md', readmeData);
+## 使用
+
+安装插件后重启 VSCode，打开对应的文件，如果有问题就会看到提示。
+
+## 更多
+
+- [前端学习路径](https://modyqyw.top/front-end/roadmap/)
+- [前端环境配置](https://modyqyw.top/front-end/environment/)
+- [前端杂项](https://modyqyw.top/front-end/misc/)
+- [fabric](https://github.com/modyqyw/fabric#readme) - 不同前端项目的可共享规范
+- [mp-scss](https://modyqyw.top/mp-scss/) - 一个基于 Flexbox 的小程序 SCSS 样式库，用于快速实现自定义设计
+`;
+
+fs.writeFileSync('README.md', readme);

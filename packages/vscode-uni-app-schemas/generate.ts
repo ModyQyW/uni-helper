dfb5cb590cc94d3a13c5f7f138db994332232211
ModyQyW <wurui-dev@foxmail.com>
2022-07-04 09:01:56 +0800

    chore: use ts to generate

---
+++ packages/vscode-uni-app-schemas/generate.ts
@@ -0,0 +1,22 @@
+import fs from 'node:fs';
+
+const readme = `# vscode-uni-app-schemas
+
+## 插件特性
+
+校验 \`uni-app\` 中的 \`androidPrivacy.json\`、\`pages.json\` 和 \`manifest.json\` 格式。
+
+**插件和文档的冲突之处，请以文档为准。**
+
+插件源代码在 [ModyQyW/uni-helper](https://github.com/ModyQyW/uni-helper)。欢迎提交 ISSUE 和 PR 改进本插件。
+
+## 使用
+
+安装插件后重启 VSCode 即可。
+
+## 额外推荐
+
+请查看 [uni-helper 插件说明](https://marketplace.visualstudio.com/items?itemName=ModyQyW.vscode-uni-helper)。
+`;
+
+fs.writeFileSync('README.md', readme);

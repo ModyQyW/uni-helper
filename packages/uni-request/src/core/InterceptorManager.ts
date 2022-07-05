eed7c8abc6018bae9cc9e323444eec27461ca04e
ModyQyW <wurui-dev@foxmail.com>
2022-07-05 08:56:13 +0800

    feat: add InterceptorManager

---
+++ packages/uni-request/src/core/InterceptorManager.ts
@@ -0,0 +1,47 @@
+import type { UrData, UrInterceptorManagerHandler, UrInterceptorOptions } from '../types';
+
+class InterceptorManager<V, T = V, D = UrData> {
+  private handlers: (UrInterceptorManagerHandler<V, T, D> | null)[];
+
+  constructor() {
+    this.handlers = [];
+  }
+
+  use(
+    fulfilled?: (value: V) => T | Promise<T>,
+    rejected?: (error: any) => any,
+    options?: UrInterceptorOptions<D>,
+  ) {
+    this.handlers.push({
+      fulfilled,
+      rejected,
+      synchronous: options?.synchronous ?? false,
+      runWhen: options?.runWhen,
+    });
+    return this.handlers.length - 1;
+  }
+
+  eject(id: number) {
+    if (this.handlers[id]) {
+      this.handlers[id] = null;
+    }
+  }
+
+  clear() {
+    if (this.handlers) {
+      this.handlers = [];
+    }
+  }
+
+  forEach(fn?: (handler: UrInterceptorManagerHandler<V, T, D>) => void) {
+    for (const handler of this.handlers) {
+      if (handler && fn) {
+        fn(handler);
+      }
+    }
+  }
+}
+
+export { InterceptorManager };
+
+export default InterceptorManager;

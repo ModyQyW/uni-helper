c21167cc3f0b07b5903737ea5ac200276cae3fe6
ModyQyW <wurui-dev@foxmail.com>
2022-07-07 08:40:46 +0800

    feat: add utils

---
+++ packages/uni-request/src/utils/buildDownloadConfig.ts
@@ -0,0 +1,15 @@
+import { UrData, UrDownloadConfig } from '../types';
+import { buildFullPath } from './buildFullPath';
+import { buildUrl } from './buildUrl';
+
+export const buildDownloadConfig = <T = UrData, D = UrData>(config: UrDownloadConfig<T, D>) =>
+  ({
+    url: buildUrl(
+      buildFullPath(config.baseUrl, config.url),
+      config.params,
+      config.paramsSerializer,
+    ),
+    header: config.headers,
+    timeout: config.timeout,
+    filePath: config.filePath,
+  } as UniApp.DownloadFileOption);
--- packages/uni-request/src/utils/index.ts
+++ packages/uni-request/src/utils/index.ts
@@ -1,4 +1,7 @@
+export * from './buildDownloadConfig';
 export * from './buildFullPath';
+export * from './buildRequestConfig';
+export * from './buildUploadConfig';
 export * from './buildUrl';
 export * from './combineUrls';
 export * from './isAbsoluteUrl';

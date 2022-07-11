66500b0e98e4ff16896d07a553a0cbff0cc4d031
ModyQyW <wurui-dev@foxmail.com>
2022-07-11 14:56:09 +0800

    feat: add settle and tests

---
+++ packages/uni-request/src/core/settle.js
@@ -0,0 +1,18 @@
+import { UrError } from './UrError';
+
+export const settle = (resolve, reject, response) => {
+  const { validateStatus } = response.config;
+  if (!response.status || !validateStatus || validateStatus(response.status)) {
+    resolve(response);
+  } else {
+    reject(
+      new UrError(
+        `Request failed with status code ${response.status}`,
+        [UrError.ERR_BAD_REQUEST, UrError.ERR_BAD_RESPONSE][Math.floor(response.status / 100) - 4],
+        response.config,
+        response.request,
+        response,
+      ),
+    );
+  }
+};

6c7a5c6b131d51433a56e05ffcb5d4fdcdbffe82
ModyQyW <wurui-dev@foxmail.com>
2022-07-12 10:36:02 +0800

    chore: vitest add coverage config

--- packages/uni-request/vitest.config.ts
+++ packages/uni-request/vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     clearMocks: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });

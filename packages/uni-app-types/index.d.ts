2b4a2d3b624e6bb91466527fd04476ea16610bca
ModyQyW <wurui-dev@foxmail.com>
2022-07-15 15:19:15 +0800

    feat: add global components

--- packages/uni-app-types/index.d.ts
+++ packages/uni-app-types/index.d.ts
@@ -1,7 +1,30 @@
 import '@vue/runtime-core';
+import {
+  Block,
+  View,
+  ScrollView,
+  Swiper,
+  SwiperItem,
+  MatchMedia,
+  MovableArea,
+  MovableView,
+  CoverView,
+  CoverImage,
+} from './types';
 
 export {};
 
 declare module '@vue/runtime-core' {
-  export interface GlobalComponents {}
+  export interface GlobalComponents {
+    Block: Block;
+    View: View;
+    ScrollView: ScrollView;
+    Swiper: Swiper;
+    SwiperItem: SwiperItem;
+    MatchMedia: MatchMedia;
+    MovableArea: MovableArea;
+    MovableView: MovableView;
+    CoverView: CoverView;
+    CoverImage: CoverImage;
+  }
 }

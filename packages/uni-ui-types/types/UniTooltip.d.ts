31e2f591bfb9ae26ad83b47add5b1463bda2ae24
ModyQyW <wurui-dev@foxmail.com>
2022-07-18 21:10:26 +0800

    feat: add uni-tooltip types

---
+++ packages/uni-ui-types/types/UniTooltip.d.ts
@@ -0,0 +1,13 @@
+import { Component } from '../Component';
+
+export interface UniTooltipProps {
+  /**
+   * @desc 显示内容
+   */
+  content: string;
+}
+
+/**
+ * @desc 提示文字
+ */
+export type UniTooltip = Component<Partial<UniTooltipProps>>;

9201e2b3cdbd04782e3d681a8def2fed4d754aa8
ModyQyW <wurui-dev@foxmail.com>
2022-07-19 11:16:47 +0800

    feat: add UniGrid types

---
+++ packages/uni-ui-types/types/UniGridItem.d.ts
@@ -0,0 +1,13 @@
+import { Component } from '../Component';
+
+export interface UniGridItemProps {
+  /**
+   * @desc 子组件唯一标识
+   */
+  index: number;
+}
+
+/**
+ * @desc 宫格项
+ */
+export type UniGridItem = Component<Partial<UniGridItemProps>>;

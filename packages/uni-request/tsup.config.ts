81f31494aa15f6ca8bf773d2eeb8e54011728ecd
ModyQyW <wurui-dev@foxmail.com>
2022-07-22 13:54:33 +0800

    chore: update tsup config

--- packages/uni-request/tsup.config.ts
+++ packages/uni-request/tsup.config.ts
@@ -4,18 +4,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: ['src/index.js'],
-    format: 'esm',
-    minify: true,
-    shims: true,
-    target: 'es5',
-    banner: {
-      js: `import {createRequire} from 'module';var require=createRequire(import\.meta.url);`,
-    },
-  },
-  {
-    entry: ['src/index.js'],
-    format: 'cjs',
-    minify: true,
+    format: ['esm', 'cjs'],
+    // minify: true,
     shims: true,
     target: 'es5',
   },

3d28aefb25204ff2c24e83d20274ee4852fa4515
ModyQyW <wurui-dev@foxmail.com>
2022-08-02 14:51:54 +0800

    feat: add useNetwork

---
+++ packages/uni-app-use/src/useNetwork/index.ts
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue-demi';
+import { tryOnScopeDispose } from '@vueuse/shared';
+
+export type NetworkType = 'ethernet' | 'none' | 'wifi' | 'unknown' | '2g' | '3g' | '4g' | '5g';
+
+export function useNetwork() {
+  const isOnline = ref(true);
+  const isOffline = computed(() => !isOnline.value);
+  const type = ref<NetworkType>('unknown');
+
+  const updateNetwork = (
+    result: UniApp.GetNetworkTypeSuccess | UniApp.OnNetworkStatusChangeSuccess,
+  ) => {
+    isOnline.value = !!result?.networkType && result?.networkType !== 'none';
+    type.value = (result?.networkType ?? 'unknown') as NetworkType;
+  };
+
+  uni.getNetworkType({
+    success: (result) => {
+      updateNetwork(result);
+    },
+  });
+
+  const callback = (result: UniApp.OnNetworkStatusChangeSuccess) => {
+    updateNetwork(result);
+  };
+
+  uni.onNetworkStatusChange(callback);
+
+  const stop = () => {
+    uni.offNetworkStatusChange(callback);
+  };
+
+  tryOnScopeDispose(stop);
+
+  return {
+    isOnline,
+    isOffline,
+    type,
+  };
+}
+
+export type UseNetworkReturn = ReturnType<typeof useNetwork>;

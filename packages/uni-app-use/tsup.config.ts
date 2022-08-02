9962d1b3e5ca3adce40b21312e8dbd3eed09cab2
ModyQyW <wurui-dev@foxmail.com>
2022-08-02 10:11:35 +0800

    chore: initial uni-app-use

---
+++ packages/uni-app-use/tsup.config.ts
@@ -0,0 +1,35 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { defineConfig } from 'tsup';
+
+const getTsFiles = (entry: string): string[] =>
+  fs
+    .readdirSync(entry)
+    .flatMap((item) => {
+      if (
+        item.includes('.ts') &&
+        !item.includes('.test.') &&
+        !item.includes('.spec.') &&
+        !item.includes('.d.') &&
+        !item.includes('type')
+      ) {
+        return path.join(entry, item);
+      }
+      if (!item.includes('.') && !item.includes('type')) {
+        return getTsFiles(path.join(entry, item));
+      }
+      return '';
+    })
+    .filter((item) => !!item) as string[];
+
+export default defineConfig([
+  {
+    entry: getTsFiles('src'),
+    format: ['esm', 'cjs'],
+    dts: true,
+    minify: true,
+    shims: true,
+    splitting: false,
+    target: 'es5',
+  },
+]);

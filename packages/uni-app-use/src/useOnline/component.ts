d23baf985c817043f8b0cfa56c4c4fa1544f5611
ModyQyW <wurui-dev@foxmail.com>
2022-08-02 14:59:17 +0800

    feat: add components

---
+++ packages/uni-app-use/src/useOnline/component.ts
@@ -0,0 +1,17 @@
+import { defineComponent, reactive } from 'vue-demi';
+import { useOnline } from '@vueuse/core';
+
+export const UseOnline = defineComponent({
+  name: 'UseOnline',
+  setup(props, { slots }) {
+    const data = reactive({
+      isOnline: useOnline(),
+    });
+
+    return () => {
+      if (slots.default) {
+        return slots.default(data);
+      }
+    };
+  },
+});

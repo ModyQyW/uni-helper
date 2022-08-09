ef39316ee4838aba2f8a823b00f7e412ba83adec
ModyQyW <wurui-dev@foxmail.com>
2022-08-09 21:34:01 +0800

    fix: fix useApp setGlobalData

--- packages/uni-app-use/src/useApp/index.ts
+++ packages/uni-app-use/src/useApp/index.ts
@@ -2,17 +2,28 @@ import { ref, computed } from 'vue-demi';
 
 export function useApp() {
   const app = ref(getApp());
-  const globalData = computed({
-    get: () => app.value.globalData,
-    set: (nextGlobalData, override = false) => {
-      app.value.globalData = override
-        ? nextGlobalData
-        : { ...app.value.globalData, ...nextGlobalData };
-    },
-  });
+  const globalData = computed(() => app.value.globalData);
+
+  function setGlobalData(nextGlobalData: AnyObject | undefined, override?: boolean): void;
+  function setGlobalData(key: string, value: any): void;
+  function setGlobalData(...args: any[]) {
+    if (typeof args[0] === 'string') {
+      const key = args[0];
+      const value = args[0];
+      app.value.globalData = { ...globalData, [key]: value };
+      return;
+    }
+    const nextGlobalData = args[0];
+    const override = args[1] ?? true;
+    app.value.globalData = {
+      ...(override ? undefined : app.value.globalData),
+      ...nextGlobalData,
+    };
+  }
 
   return {
     app,
     globalData,
+    setGlobalData,
   };
 }

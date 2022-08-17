cd00a7028ff3e35d68ce6f181ae65587adde9f9f
ModyQyW <wurui-dev@foxmail.com>
2022-08-17 16:51:41 +0800

    perf: 调整 useSupported 实现

--- packages/uni-app-use/src/useSupported/index.ts
+++ packages/uni-app-use/src/useSupported/index.ts
@@ -1,13 +1,14 @@
+import { MaybeComputedRef, resolveUnref } from '@vueuse/shared';
 import { Ref, ref } from 'vue-demi';
 
 /**
  * Get api is supported
  */
-export function useSupported(api: string) {
+export function useSupported(api: MaybeComputedRef<string>) {
   const isSupported = ref() as Ref<boolean>;
 
   const update = () => {
-    isSupported.value = uni.canIUse(api);
+    isSupported.value = uni.canIUse(resolveUnref(api));
   };
 
   update();

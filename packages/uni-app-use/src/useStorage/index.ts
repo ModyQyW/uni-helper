bafebb59c9d3ab6fb91675ff9c5dac5849ee258d
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 15:27:09 +0800

    perf: update useStorage types

--- packages/uni-app-use/src/useStorage/index.ts
+++ packages/uni-app-use/src/useStorage/index.ts
@@ -1,18 +1,36 @@
+import { reactive } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+
+export interface UniGetStorageOptions extends UniApp.GetStorageOptions {}
+export type GetStorageOptions = MaybeComputedRef<UniGetStorageOptions>;
+
+export interface UniSetStorageOptions extends UniApp.SetStorageOptions {}
+export type SetStorageOptions = MaybeComputedRef<UniSetStorageOptions>;
+
+export interface UniRemoveStorageOptions extends UniApp.RemoveStorageOptions {}
+export type RemoveStorageOptions = MaybeComputedRef<UniRemoveStorageOptions>;
+
+export interface UniGetStorageInfoOptions extends UniApp.GetStorageInfoOptions {}
+export type GetStorageInfoOptions = MaybeComputedRef<UniGetStorageInfoOptions>;
+
 /**
  * Get storage info
  */
 export function useStorage() {
-  const getStorage = (options: UniApp.GetStorageOptions) => uni.getStorage(options);
+  const getStorage = (options?: GetStorageOptions) =>
+    uni.getStorage(reactive({ key: '', ...resolveUnref(options) }));
   const getStorageSync = (key: string) => uni.getStorageSync(key);
   const get = getStorage;
   const getSync = getStorageSync;
 
-  const setStorage = (options: UniApp.SetStorageOptions) => uni.setStorage(options);
+  const setStorage = (options?: SetStorageOptions) =>
+    uni.setStorage(reactive({ key: '', data: undefined, ...resolveUnref(options) }));
   const setStorageSync = (key: string, value: any) => uni.setStorageSync(key, value);
   const set = setStorage;
   const setSync = setStorageSync;
 
-  const removeStorage = (options: UniApp.RemoveStorageOptions) => uni.removeStorage(options);
+  const removeStorage = (options?: RemoveStorageOptions) =>
+    uni.removeStorage(reactive({ key: '', ...resolveUnref(options) }));
   const removeStorageSync = (key: string) => uni.removeStorageSync(key);
   const remove = removeStorage;
   const removeSync = removeStorageSync;
@@ -22,7 +40,8 @@ export function useStorage() {
   const clear = clearStorage;
   const clearSync = clearStorageSync;
 
-  const getStorageInfo = (options: UniApp.GetStorageInfoOptions) => uni.getStorageInfo(options);
+  const getStorageInfo = (options?: GetStorageInfoOptions) =>
+    uni.getStorageInfo(reactive({ ...options }));
   const getStorageInfoSync = () => uni.getStorageInfoSync();
   const getInfo = getStorageInfo;
   const getInfoSync = getStorageInfoSync;

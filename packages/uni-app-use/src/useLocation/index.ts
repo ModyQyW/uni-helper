5fc7a22cd18a1a26971b29661892723ab99477e9
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 14:39:17 +0800

    perf: update useLocation types

--- packages/uni-app-use/src/useLocation/index.ts
+++ packages/uni-app-use/src/useLocation/index.ts
@@ -1,6 +1,27 @@
+import { reactive } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+
+export interface UniGetLocationOptions extends UniApp.GetLocationOptions {}
+export type GetLocationOptions = MaybeComputedRef<UniGetLocationOptions>;
+
+export interface UniChooseLocationOptions extends UniApp.ChooseLocationOptions {}
+export type ChooseLocationOptions = MaybeComputedRef<UniChooseLocationOptions>;
+
+export interface UniOpenLocationOptions extends UniApp.OpenLocationOptions {}
+export type OpenLocationOptions = MaybeComputedRef<UniOpenLocationOptions>;
+
 export function useLocation() {
-  const getLocation = (options: UniApp.GetLocationOptions) => uni.getLocation(options);
-  const chooseLocation = (options: UniApp.ChooseLocationOptions) => uni.chooseLocation(options);
-  const openLocation = (options: UniApp.OpenLocationOptions) => uni.openLocation(options);
-  return { getLocation, chooseLocation, openLocation };
+  const getLocation = (options?: GetLocationOptions) =>
+    uni.getLocation(reactive({ ...resolveUnref(options) }));
+  const get = getLocation;
+
+  const chooseLocation = (options?: ChooseLocationOptions) =>
+    uni.chooseLocation(reactive({ ...resolveUnref(options) }));
+  const choose = chooseLocation;
+
+  const openLocation = (options?: OpenLocationOptions) =>
+    uni.openLocation(reactive({ latitude: 0, longitude: 0, ...resolveUnref(options) }));
+  const open = openLocation;
+
+  return { getLocation, get, chooseLocation, choose, openLocation, open };
 }

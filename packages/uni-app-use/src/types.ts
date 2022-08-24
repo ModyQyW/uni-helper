2806661f8e1454c2de0b4700047f120e158f2a05
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 10:56:44 +0800

    feat: add types

--- packages/uni-app-use/src/types.ts
+++ packages/uni-app-use/src/types.ts
@@ -1,1 +1,12 @@
+import { Ref } from 'vue';
+import { MaybeRef } from '@vueuse/core';
+
 export type Parameter<T extends (args: any) => any> = T extends (args: infer P) => any ? P : never;
+
+export type MakeRef<T extends Record<string, any>> = {
+  [K in keyof T]: Ref<T[K]>;
+};
+
+export type MakeMaybeRef<T extends Record<string, any>> = {
+  [K in keyof T]: MaybeRef<T[K]>;
+};

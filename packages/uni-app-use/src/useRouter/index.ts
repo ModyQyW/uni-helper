863f2d86e5cf6d821f7fc8034ae2606221cc4e47
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 15:13:56 +0800

    perf: update useRouter types

--- packages/uni-app-use/src/useRouter/index.ts
+++ packages/uni-app-use/src/useRouter/index.ts
@@ -1,5 +1,26 @@
-import { tryOnScopeDispose } from '@vueuse/core';
-import { ref, computed } from 'vue';
+import { tryOnScopeDispose, MaybeComputedRef, resolveUnref } from '@vueuse/core';
+import { reactive, ref, computed } from 'vue';
+
+export interface UniReLaunchOptions extends UniApp.ReLaunchOptions {}
+export type ReLaunchOptions = MaybeComputedRef<UniReLaunchOptions>;
+
+export interface UniSwitchTabOptions extends UniApp.SwitchTabOptions {}
+export type SwitchTabOptions = MaybeComputedRef<UniSwitchTabOptions>;
+
+export interface UniRedirectToOptions extends UniApp.RedirectToOptions {}
+export type RedirectToOptions = MaybeComputedRef<UniRedirectToOptions>;
+
+export interface UniNavigateToOptions extends UniApp.NavigateToOptions {}
+export type NavigateToOptions = MaybeComputedRef<UniNavigateToOptions>;
+
+export interface UniNavigateBackOptions extends UniApp.NavigateBackOptions {}
+export type NavigateBackOptions = MaybeComputedRef<UniNavigateBackOptions>;
+
+export interface UniNavigateToMiniProgramOptions extends UniApp.NavigateToMiniProgramOptions {}
+export type NavigateToMiniProgramOptions = MaybeComputedRef<UniNavigateToMiniProgramOptions>;
+
+export interface UniNavigateBackMiniProgramOptions extends UniApp.NavigateBackMiniProgramOptions {}
+export type NavigateBackMiniProgramOptions = MaybeComputedRef<UniNavigateBackMiniProgramOptions>;
 
 /**
  * Get router info
@@ -16,15 +37,26 @@ export function useRouter() {
   const route = computed(() => page.value?.route);
   const prevRoute = computed(() => prevPage.value?.route);
 
-  const reLaunch = (options: UniApp.ReLaunchOptions) => uni.reLaunch(options);
-  const switchTab = (options: UniApp.SwitchTabOptions) => uni.switchTab(options);
-  const redirectTo = (options: UniApp.RedirectToOptions) => uni.redirectTo(options);
-  const navigateTo = (options: UniApp.NavigateToOptions) => uni.navigateTo(options);
-  const navigateBack = (options: UniApp.NavigateBackOptions) => uni.navigateBack(options);
-  const navigateToMiniprogram = (options: UniApp.NavigateToMiniProgramOptions) =>
-    uni.navigateToMiniProgram(options);
-  const navigateBackMiniprogram = (options: UniApp.NavigateBackMiniProgramOptions) =>
-    uni.navigateBackMiniProgram(options);
+  const reLaunch = (options?: ReLaunchOptions) =>
+    uni.reLaunch(reactive({ url: '', ...resolveUnref(options) }));
+
+  const switchTab = (options?: SwitchTabOptions) =>
+    uni.switchTab(reactive({ url: '', ...resolveUnref(options) }));
+
+  const redirectTo = (options?: RedirectToOptions) =>
+    uni.redirectTo(reactive({ url: '', ...resolveUnref(options) }));
+
+  const navigateTo = (options?: NavigateToOptions) =>
+    uni.navigateTo(reactive({ url: '', ...resolveUnref(options) }));
+
+  const navigateBack = (options?: NavigateBackOptions) =>
+    uni.navigateBack(reactive({ ...resolveUnref(options) }));
+
+  const navigateToMiniProgram = (options?: NavigateToMiniProgramOptions) =>
+    uni.navigateToMiniProgram(reactive({ appId: '', ...resolveUnref(options) }));
+
+  const navigateBackMiniProgram = (options?: NavigateBackMiniProgramOptions) =>
+    uni.navigateBackMiniProgram(reactive({ ...resolveUnref(options) }));
 
   return {
     pages,
@@ -37,7 +69,7 @@ export function useRouter() {
     redirectTo,
     navigateTo,
     navigateBack,
-    navigateToMiniprogram,
-    navigateBackMiniprogram,
+    navigateToMiniProgram,
+    navigateBackMiniProgram,
   };
 }

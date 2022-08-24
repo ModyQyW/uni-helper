f8ae12840b89a8e0ecde196296357db57822f3a9
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 10:57:04 +0800

    perf: 优化 `useActionSheet` 类型

--- packages/uni-app-use/src/useActionSheet/index.ts
+++ packages/uni-app-use/src/useActionSheet/index.ts
@@ -1,15 +1,20 @@
+import { reactive } from 'vue';
+import { MakeMaybeRef } from '../types';
+
 export interface ShowActionSheetOptions
   extends Omit<UniApp.ShowActionSheetOptions, 'title' | 'itemList'> {
   itemList: string[];
 }
 
-export function useActionSheet(options?: ShowActionSheetOptions) {
-  const showActionSheet = (newOptions?: ShowActionSheetOptions) => {
-    uni.showActionSheet({
-      itemList: [],
-      ...options,
-      ...newOptions,
-    });
+export function useActionSheet(options?: MakeMaybeRef<ShowActionSheetOptions>) {
+  const showActionSheet = (newOptions?: MakeMaybeRef<ShowActionSheetOptions>) => {
+    uni.showActionSheet(
+      reactive({
+        itemList: [],
+        ...options,
+        ...newOptions,
+      }),
+    );
   };
   return showActionSheet;
 }

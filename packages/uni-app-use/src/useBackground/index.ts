804ad488401e8a35e7377eeb2ba00c4646e700d7
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 10:57:23 +0800

    perf: 优化 `useBackground` 返回的类型

--- packages/uni-app-use/src/useBackground/index.ts
+++ packages/uni-app-use/src/useBackground/index.ts
@@ -1,8 +1,13 @@
+import { reactive } from 'vue';
+import { MakeMaybeRef } from '../types';
+
 export function useBackground() {
-  const setBackgroundColor = (options: UniApp.SetBackgroundColorOptions) =>
-    uni.setBackgroundColor(options);
-  const setBackgroundTextStyle = (options: UniApp.SetBackgroundTextStyleOptions) =>
-    uni.setBackgroundTextStyle(options);
+  const setBackgroundColor = (options: MakeMaybeRef<UniApp.SetBackgroundColorOptions>) =>
+    uni.setBackgroundColor(reactive(options));
+
+  const setBackgroundTextStyle = (options: MakeMaybeRef<UniApp.SetBackgroundTextStyleOptions>) =>
+    uni.setBackgroundTextStyle(reactive(options));
+
   return {
     setBackgroundColor,
     setBackgroundTextStyle,

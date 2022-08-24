bf66a710b9c30ff98b0e272a9dcc9868377a31f9
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 10:23:21 +0800

    perf: 优化 useActionSheet 类型

--- packages/uni-app-use/src/useActionSheet/index.ts
+++ packages/uni-app-use/src/useActionSheet/index.ts
@@ -1,6 +1,12 @@
-export function useActionSheet(options: UniApp.ShowActionSheetOptions) {
-  const showActionSheet = (newOptions: UniApp.ShowActionSheetOptions) => {
+export interface ShowActionSheetOptions
+  extends Omit<UniApp.ShowActionSheetOptions, 'title' | 'itemList'> {
+  itemList: string[];
+}
+
+export function useActionSheet(options?: ShowActionSheetOptions) {
+  const showActionSheet = (newOptions?: ShowActionSheetOptions) => {
     uni.showActionSheet({
+      itemList: [],
       ...options,
       ...newOptions,
     });

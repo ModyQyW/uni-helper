6712014f7da2d1416d61088aa24201e902b49129
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 13:08:22 +0800

    perf: 优化 `useGlobalData` 类型

--- packages/uni-app-use/src/useGlobalData/index.ts
+++ packages/uni-app-use/src/useGlobalData/index.ts
@@ -1,28 +1,29 @@
-import { computed } from 'vue';
+import { computed, reactive } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
 import { useApp } from '../useApp';
 
 /**
  * Get and set the globalData of the current application instance
  */
 export function useGlobalData() {
-  const { app } = useApp();
+  const app = useApp();
 
   const globalData = computed(() => app.value.globalData);
 
-  function setGlobalData(nextGlobalData: AnyObject | undefined): void;
+  function setGlobalData(nextGlobalData: MaybeComputedRef<AnyObject | undefined>): void;
   function setGlobalData(key: string, value: any): void;
   function setGlobalData(...args: any[]) {
     if (typeof args[0] === 'string') {
       const key = args[0];
       const value = args[1];
-      app.value.globalData = { ...globalData, [key]: value };
+      app.value.globalData = reactive({ ...globalData.value, [key]: value });
       return;
     }
     const nextGlobalData = args[0];
-    app.value.globalData = {
-      ...app.value.globalData,
-      ...nextGlobalData,
-    };
+    app.value.globalData = reactive({
+      ...globalData.value,
+      ...resolveUnref(nextGlobalData),
+    });
   }
 
   return {

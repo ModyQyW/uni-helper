c853256978498b05cee7af64786338f9cf4bf6ce
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 15:41:49 +0800

    perf: update useToast types

--- packages/uni-app-use/src/useToast/index.ts
+++ packages/uni-app-use/src/useToast/index.ts
@@ -1,9 +1,17 @@
-export function useToast(options: UniApp.ShowToastOptions) {
-  const showToast = (newOptions: UniApp.ShowToastOptions) => {
-    uni.showToast({
-      ...options,
-      ...newOptions,
-    });
+import { reactive } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+
+export interface UniShowToastOptions extends UniApp.ShowToastOptions {}
+export type ShowToastOptions = MaybeComputedRef<UniShowToastOptions>;
+
+export function useToast(options?: ShowToastOptions) {
+  const showToast = (newOptions?: ShowToastOptions) => {
+    uni.showToast(
+      reactive({
+        ...resolveUnref(options),
+        ...resolveUnref(newOptions),
+      }),
+    );
   };
   const hideToast = () => uni.hideToast();
   return { showToast, hideToast };

23ce37d5640618d4e96bfdfdd9f3df615f4cca53
ModyQyW <wurui-dev@foxmail.com>
2022-08-24 14:42:44 +0800

    perf: update useModal types

--- packages/uni-app-use/src/useModal/index.ts
+++ packages/uni-app-use/src/useModal/index.ts
@@ -1,9 +1,17 @@
-export function useModal(options: UniApp.ShowModalOptions) {
-  const showModal = (newOptions: UniApp.ShowModalOptions) => {
-    uni.showModal({
-      ...options,
-      ...newOptions,
-    });
+import { reactive } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+
+export interface UniShowModalOptions extends UniApp.ShowModalOptions {}
+export type ShowModalOptions = MaybeComputedRef<UniShowModalOptions>;
+
+export function useModal(options?: ShowModalOptions) {
+  const showModal = (newOptions?: ShowModalOptions) => {
+    uni.showModal(
+      reactive({
+        ...resolveUnref(options),
+        ...resolveUnref(newOptions),
+      }),
+    );
   };
   return showModal;
 }

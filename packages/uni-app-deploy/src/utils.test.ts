a7dfb0251deebf5e29e4ffeb821922991455b145
ModyQyW <wurui-dev@foxmail.com>
2022-09-27 09:59:11 +0800

    test: add utils test

---
+++ packages/uni-app-deploy/src/utils.test.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import { describe, it, expect } from 'vitest';
+import { getCwd, getFileDir, getFileField, getFilePath } from './utils';
+
+describe('utils::mp-weixin', () => {
+  it('getCwd', () => {
+    expect(getCwd({})).toBe(process.cwd());
+    expect(getCwd({ cwd: '..' })).toBe('..');
+  });
+
+  it('getFileField', () => {
+    expect(
+      getFileField({}, [
+        { entry: ['src', 'manifest.json'], prop: ['mp-weixin', 'appid'] },
+        { entry: ['**', 'manifest.json'], prop: ['mp-weixin', 'appid'] },
+      ]),
+    ).toBe('touristappid');
+    expect(() =>
+      getFileField({}, [{ entry: ['not-exist.file'], prop: ['not', 'exist', 'prop'] }]),
+    ).toThrowError('Can not get files.');
+  });
+
+  it('getFilePath', () => {
+    expect(
+      getFilePath({}, [{ entry: ['src', 'manifest.json'] }, { entry: ['**', 'manifest.json'] }]),
+    ).toBe(path.resolve('src', 'manifest.json'));
+    expect(() => getFilePath({}, [{ entry: ['not-exist.file'] }])).toThrowError(
+      'Can not get files.',
+    );
+  });
+
+  it('getFileDir', () => {
+    expect(
+      getFileDir({}, [{ entry: ['src', 'manifest.json'] }, { entry: ['**', 'manifest.json'] }]),
+    ).toBe(path.resolve('src'));
+    expect(() => getFileDir({}, [{ entry: ['not-exist.file'] }])).toThrowError(
+      'Can not get files.',
+    );
+  });
+});

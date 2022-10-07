9d6416d83d5dfdb05c2f1e9b552c617ca94c9e25
ModyQyW <wurui-dev@foxmail.com>
2022-10-07 20:32:13 +0800

    perf: improve status

--- packages/uni-app-network/src/defaults/index.ts
+++ packages/uni-app-network/src/defaults/index.ts
@@ -1,7 +1,8 @@
-import { UanBaseConfig } from '../types';
+import { HttpStatusCode, UanBaseConfig } from '../types';
 
 export const defaults: Partial<UanBaseConfig> = {
   adapter: 'request',
   timeout: 0,
-  validateStatus: (status) => status >= 200 && status < 300,
+  validateStatus: (status) =>
+    status >= HttpStatusCode.Ok && status < HttpStatusCode.MultipleChoices,
 };

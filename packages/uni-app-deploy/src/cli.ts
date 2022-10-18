ff5dc5a692b9c26ee810419c5d3091e98972df0b
ModyQyW <wurui-dev@foxmail.com>
2022-10-18 21:19:08 +0800

    fix: fix hashbeng

--- packages/uni-app-deploy/src/cli.ts
+++ packages/uni-app-deploy/src/cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#! /usr/bin/env node
 import { Command } from 'commander';
 import pkg from '../package.json';
 import { defineConfig, loadConfig, UniAppDeployConfig } from './config';

4d83787e72ca7a4d2abf1d285f35f23c919510d8
ModyQyW <wurui-dev@foxmail.com>
2022-10-24 10:38:49 +0800

    feat: add useLocation exports

--- packages/uni-app-use/src/useLocation/index.ts
+++ packages/uni-app-use/src/useLocation/index.ts
@@ -1,5 +1,6 @@
 import { reactive } from 'vue';
 import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+import { Parameter } from '../types';
 
 export interface UniGetLocationOptions extends UniApp.GetLocationOptions {}
 export type GetLocationOptions = MaybeComputedRef<UniGetLocationOptions>;
@@ -10,18 +11,63 @@ export type ChooseLocationOptions = MaybeComputedRef<UniChooseLocationOptions>;
 export interface UniOpenLocationOptions extends UniApp.OpenLocationOptions {}
 export type OpenLocationOptions = MaybeComputedRef<UniOpenLocationOptions>;
 
+export interface UniStartLocationUpdateOptions extends UniApp.StartLocationUpdateOption {}
+export type StartLocationUpdateOptions = MaybeComputedRef<UniStartLocationUpdateOptions>;
+
+export interface UniStartLocationUpdateBackgroundOptions
+  extends UniApp.StartLocationUpdateBackgroundOption {}
+export type StartLocationUpdateBackgroundOptions =
+  MaybeComputedRef<UniStartLocationUpdateBackgroundOptions>;
+
+export interface UniStopLocationUpdateOptions extends UniApp.StartLocationUpdateOption {}
+export type StopLocationUpdateOptions = MaybeComputedRef<UniStopLocationUpdateOptions>;
+
 export function useLocation() {
   const getLocation = (options?: GetLocationOptions) =>
     uni.getLocation(reactive({ ...resolveUnref(options) }));
-  const get = getLocation;
 
   const chooseLocation = (options?: ChooseLocationOptions) =>
     uni.chooseLocation(reactive({ ...resolveUnref(options) }));
-  const choose = chooseLocation;
 
   const openLocation = (options?: OpenLocationOptions) =>
     uni.openLocation(reactive({ latitude: 0, longitude: 0, ...resolveUnref(options) }));
-  const open = openLocation;
 
-  return { getLocation, get, chooseLocation, choose, openLocation, open };
+  const onLocationChange = (callback: Parameter<UniApp.Uni['onLocationChange']>) =>
+    uni.onLocationChange(callback);
+
+  const onLocationChangeError = (callback: Parameter<UniApp.Uni['onLocationChangeError']>) =>
+    uni.onLocationChangeError(callback);
+
+  const offLocationChange = (callback: Parameter<UniApp.Uni['offLocationChange']>) =>
+    uni.offLocationChange(callback);
+
+  const startLocationUpdate = (options?: StartLocationUpdateOptions) =>
+    uni.startLocationUpdate(reactive({ ...resolveUnref(options) }));
+
+  const startLocationBackgroundUpdate = (options?: StartLocationUpdateBackgroundOptions) =>
+    uni.startLocationUpdateBackground(reactive({ ...resolveUnref(options) }));
+
+  const stopLocationUpdate = (options?: StopLocationUpdateOptions) =>
+    uni.stopLocationUpdate(reactive({ ...resolveUnref(options) }));
+
+  return {
+    getLocation,
+    get: getLocation,
+    chooseLocation,
+    choose: chooseLocation,
+    openLocation,
+    open: openLocation,
+    onLocationChange,
+    onChange: onLocationChange,
+    onLocationChangeError,
+    onChangeError: onLocationChangeError,
+    offLocationChange,
+    offChange: offLocationChange,
+    startLocationUpdate,
+    startUpdate: startLocationUpdate,
+    startLocationBackgroundUpdate,
+    startUpdateBackground: startLocationBackgroundUpdate,
+    stopLocationUpdate,
+    stopUpdate: stopLocationUpdate,
+  };
 }

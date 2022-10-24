82b95a8acadced2a1fe71e0a258cdbdbfd0c9ac3
ModyQyW <wurui-dev@foxmail.com>
2022-10-24 09:59:46 +0800

    perf: update useVideo export

--- packages/uni-app-use/src/useVideo/index.ts
+++ packages/uni-app-use/src/useVideo/index.ts
@@ -25,44 +25,38 @@ export function useVideo() {
     });
     return context;
   };
-  const createContext = createVideoContext;
 
   const chooseVideo = (options?: ChooseVideoOptions) =>
     uni.chooseVideo(reactive({ ...resolveUnref(options) }));
-  const choose = chooseVideo;
 
   const chooseMedia = (options?: ChooseMediaOptions) =>
     uni.chooseMedia(reactive({ ...resolveUnref(options) }));
 
   const saveVideoToPhotosAlbum = (options?: SaveVideoToPhotosAlbumOptions) =>
     uni.saveVideoToPhotosAlbum(reactive({ filePath: '', ...resolveUnref(options) }));
-  const saveToPhotosAlbum = saveVideoToPhotosAlbum;
 
   const getVideoInfo = (options?: GetVideoInfoOptions) =>
     uni.getVideoInfo(reactive({ src: '', ...resolveUnref(options) }));
-  const getInfo = getVideoInfo;
 
   const compressVideo = (options?: CompressVideoOptions) =>
     uni.compressVideo(reactive({ src: '', ...resolveUnref(options) }));
-  const compress = compressVideo;
 
   const openVideoEditor = (options?: OpenVideoEditorOptions) =>
     uni.openVideoEditor(reactive({ filePath: '', ...resolveUnref(options) }));
-  const openEditor = openVideoEditor;
 
   return {
     createVideoContext,
-    createContext,
+    createContext: createVideoContext,
     chooseVideo,
-    choose,
+    choose: chooseVideo,
     chooseMedia,
     saveVideoToPhotosAlbum,
-    saveToPhotosAlbum,
+    saveToPhotosAlbum: saveVideoToPhotosAlbum,
     getVideoInfo,
-    getInfo,
+    getInfo: getVideoInfo,
     compressVideo,
-    compress,
+    compress: compressVideo,
     openVideoEditor,
-    openEditor,
+    openEditor: openVideoEditor,
   };
 }

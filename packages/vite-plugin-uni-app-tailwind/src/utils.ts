2680bab99b0ac97244984097d821923ff6f9d598
ModyQyW <wurui-dev@foxmail.com>
2022-11-03 15:53:39 +0800

    perf: imporve default getShouldApply

--- packages/vite-plugin-uni-app-tailwind/src/utils.ts
+++ packages/vite-plugin-uni-app-tailwind/src/utils.ts
@@ -1,21 +1,9 @@
 import { Options } from './options';
 
 export const getShouldApply = (targets: string[], current: string) =>
-  targets.some((item) => {
-    if (
-      (item === 'QUICKAPP' || item.startsWith('QUICKAPP-')) &&
-      (current === 'APP' || current.startsWith('APP-'))
-    ) {
-      return false;
-    }
-    if (
-      (current === 'QUICKAPP' || current.startsWith('QUICKAPP-')) &&
-      (item === 'APP' || item.startsWith('APP-'))
-    ) {
-      return false;
-    }
-    return item === current || item.includes(current) || current.includes(item);
-  });
+  targets.some(
+    (item) => item === current || item.startsWith(`${current}-`) || current.startsWith(`${item}-`),
+  );
 
 export const replaceCharacters = (source: string, options: Options, type: 'babel' | 'postcss') => {
   let newSource = source;

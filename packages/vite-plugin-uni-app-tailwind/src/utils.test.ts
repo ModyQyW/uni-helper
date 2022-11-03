65160c07af3f2a1644437de7283a44f85cb24bcf
ModyQyW <wurui-dev@foxmail.com>
2022-11-03 15:54:21 +0800

    test: add more tests

--- packages/vite-plugin-uni-app-tailwind/src/utils.test.ts
+++ packages/vite-plugin-uni-app-tailwind/src/utils.test.ts
@@ -21,15 +21,35 @@ describe('utils::getShouldApply', () => {
   });
 
   it('APP', () => {
+    expect(getShouldApply(['APP'], 'APP')).toBe(true);
     expect(getShouldApply(['APP'], 'APP-PLUS')).toBe(true);
-    expect(getShouldApply(['APP'], 'QUICKAPP')).toBe(false);
-    expect(getShouldApply(['APP-PLUS'], 'APP-PLUS')).toBe(true);
     expect(getShouldApply(['APP'], 'APP-PLUS-NVUE')).toBe(true);
+    expect(getShouldApply(['APP'], 'QUICKAPP')).toBe(false);
+    expect(getShouldApply(['APP'], 'QUICKAPP-WEBVIEW')).toBe(false);
+    expect(getShouldApply(['APP'], 'QUICKAPP-WEBVIEW-UNION')).toBe(false);
+    expect(getShouldApply(['APP'], 'QUICKAPP-WEBVIEW-HUAWEI')).toBe(false);
+    expect(getShouldApply(['APP-PLUS'], 'APP')).toBe(true);
+    expect(getShouldApply(['APP-PLUS'], 'APP-PLUS')).toBe(true);
+    expect(getShouldApply(['APP-PLUS'], 'APP-PLUS-NVUE')).toBe(true);
+    expect(getShouldApply(['APP-PLUS'], 'QUICKAPP')).toBe(false);
+    expect(getShouldApply(['APP-PLUS'], 'QUICKAPP-WEBVIEW')).toBe(false);
+    expect(getShouldApply(['APP-PLUS'], 'QUICKAPP-WEBVIEW-UNION')).toBe(false);
+    expect(getShouldApply(['APP-PLUS'], 'QUICKAPP-WEBVIEW-HUAWEI')).toBe(false);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'APP')).toBe(true);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'APP-PLUS')).toBe(true);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'APP-PLUS-NVUE')).toBe(true);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'QUICKAPP')).toBe(false);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'QUICKAPP-WEBVIEW')).toBe(false);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'QUICKAPP-WEBVIEW-UNION')).toBe(false);
+    expect(getShouldApply(['APP-PLUS-NVUE'], 'QUICKAPP-WEBVIEW-HUAWEI')).toBe(false);
   });
 
   it('QUICKAPP', () => {
     expect(getShouldApply(['QUICKAPP'], 'QUICKAPP-WEBVIEW')).toBe(true);
     expect(getShouldApply(['QUICKAPP'], 'QUICKAPP-WEBVIEW-UNION')).toBe(true);
     expect(getShouldApply(['QUICKAPP'], 'QUICKAPP-WEBVIEW-HUAWEI')).toBe(true);
+    expect(getShouldApply(['QUICKAPP'], 'APP')).toBe(false);
+    expect(getShouldApply(['QUICKAPP'], 'APP-PLUS')).toBe(false);
+    expect(getShouldApply(['QUICKAPP'], 'APP-PLUS-NVUE')).toBe(false);
   });
 });

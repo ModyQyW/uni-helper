465610966cbe060430fd36d2187533af06db59cd
ModyQyW <wurui-dev@foxmail.com>
2022-11-05 22:04:27 +0800

    test: update fixtures

--- packages/uni-app-deploy/src/fixtures/uni-app-deploy.config.ts
+++ packages/uni-app-deploy/src/fixtures/uni-app-deploy.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '../config';
 export default defineConfig({
   from: 'uni-app-deploy.config.ts',
   hello: {
-    unconfig: 'uni-app-deploy.config.ts',
+    config: 'uni-app-deploy.config.ts',
   },
 });
